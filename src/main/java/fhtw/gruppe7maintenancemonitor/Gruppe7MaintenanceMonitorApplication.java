b53abd77a74eb121d125b2d2247c7ed376019e76
ic20b501 <ic20b501@technikum-wien.at>
2023-01-08 20:00:40 +0100

    Initial commit

---
+++ src/main/java/fhtw/gruppe7maintenancemonitor/Gruppe7MaintenanceMonitorApplication.java
@@ -0,0 +1,13 @@
+package fhtw.gruppe7maintenancemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruppe7MaintenanceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gruppe7MaintenanceMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fhtw/gruppe7maintenancemonitor/Gruppe7MaintenanceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.gruppe7maintenancemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruppe7MaintenanceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
