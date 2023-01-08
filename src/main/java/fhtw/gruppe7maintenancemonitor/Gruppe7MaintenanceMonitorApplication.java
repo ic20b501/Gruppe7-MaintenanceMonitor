603c031f974442ffacff7308d5e890c03227a91e
ic21b131 <ic21b131@technikum-wien.at>
2023-01-08 20:40:20 +0100

    Kommentar

--- src/main/java/fhtw/gruppe7maintenancemonitor/Gruppe7MaintenanceMonitorApplication.java
+++ src/main/java/fhtw/gruppe7maintenancemonitor/Gruppe7MaintenanceMonitorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Gruppe7MaintenanceMonitorApplication { // Test
+public class Gruppe7MaintenanceMonitorApplication { // Test - Gueleroglu
 
     public static void main(String[] args) {
         SpringApplication.run(Gruppe7MaintenanceMonitorApplication.class, args);
