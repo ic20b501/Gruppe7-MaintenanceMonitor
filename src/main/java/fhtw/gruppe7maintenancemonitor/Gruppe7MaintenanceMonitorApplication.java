2b820bb4109616d69fd6504add46be421881b6ee
Nermin Coric <ic22b080@technikum-wien.at>
2023-01-08 20:32:56 +0100

    Testcommit

--- src/main/java/fhtw/gruppe7maintenancemonitor/Gruppe7MaintenanceMonitorApplication.java
+++ src/main/java/fhtw/gruppe7maintenancemonitor/Gruppe7MaintenanceMonitorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Gruppe7MaintenanceMonitorApplication {
+public class Gruppe7MaintenanceMonitorApplication { // Test
 
     public static void main(String[] args) {
         SpringApplication.run(Gruppe7MaintenanceMonitorApplication.class, args);
