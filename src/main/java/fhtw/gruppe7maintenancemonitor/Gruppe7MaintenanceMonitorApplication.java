eeab77fad2af157173615552784c142eb1645455
Nermin Coric <ic22b080@technikum-wien.at>
2023-01-09 20:20:57 +0100

    Branchstruktur eingerichtet

--- src/main/java/fhtw/gruppe7maintenancemonitor/Gruppe7MaintenanceMonitorApplication.java
+++ src/main/java/fhtw/gruppe7maintenancemonitor/Gruppe7MaintenanceMonitorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Gruppe7MaintenanceMonitorApplication { // Test - Gueleroglu.ic21b131
+public class Gruppe7MaintenanceMonitorApplication { // Branchstruktur eingerichtet
 
     public static void main(String[] args) {
         SpringApplication.run(Gruppe7MaintenanceMonitorApplication.class, args);
