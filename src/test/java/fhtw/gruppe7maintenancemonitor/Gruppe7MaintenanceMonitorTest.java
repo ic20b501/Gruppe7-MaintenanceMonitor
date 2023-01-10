cd599e98c0e21de398779cfb5258df379e7707d0
Nermin Coric <ic22b080@technikum-wien.at>
2023-01-10 11:14:39 +0100

    2 Junit Tests

---
+++ src/test/java/fhtw/gruppe7maintenancemonitor/Gruppe7MaintenanceMonitorTest.java
@@ -0,0 +1,34 @@
+package fhtw.gruppe7maintenancemonitor;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Gruppe7MaintenanceMonitorTest {
+
+    @Test
+    void emptyString() {
+
+        Gruppe7MaintenanceMonitorController controller = new Gruppe7MaintenanceMonitorController();
+        String msgError = "Error while setting Maintenance";
+
+        var result = controller.maintenance("");
+
+        // A - Assert
+        assertEquals("<html>  <body style=\"background-color:red;\"><header><title>Gruppe7</title></header> " +
+                "          <body><p style=\"text-align:center\">" + msgError +  "</p></body>  </html>", result);
+    }
+
+    @Test
+    void notemptyString() {
+
+        Gruppe7MaintenanceMonitorController controller = new Gruppe7MaintenanceMonitorController();
+        String message = "Maintenance until 6:00";
+
+        var result = controller.maintenance(message);
+
+        // A - Assert
+        assertEquals("<html>  <body style=\"background-color:green;\"><header><title>Gruppe7</title></header> " +
+                "          <body><p style=\"text-align:center\">" + message + " </p></body>  </html>", result);
+    }
+
+}
